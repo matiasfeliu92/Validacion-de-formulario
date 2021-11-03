1d37b37c42a31a8959cc9167fc6b379f9e175fb5
matiasfeliu92 <matumazparrote@gmail.com>
2021-11-03 10:45:36 -0300

    correcciones app JS

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+//  SELECTORES
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
+//  EXPRESIONES REGULARES
 const expresiones = {
     usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
     nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
@@ -22,21 +24,12 @@ const validarFormulario = (e) => {
         case "usuario":
             validarCampo(expresiones.usuario, e.target, 'usuario');
             break;
-        case "nombre":
-            validarCampo(expresiones.nombre, e.target, 'nombre');
-            break;
         case "password":
             validarCampo(expresiones.password, e.target, 'password');
             validarPassword2();
             break;
-        case "password2":
-            validarPassword2();
-            break;
         case "correo":
             validarCampo(expresiones.correo, e.target, 'correo');
-            break;
-        case "telefono":
-            validarCampo(expresiones.telefono, e.target, 'telefono');
             break;
     }
 }
@@ -59,26 +52,26 @@ const validarCampo = (expresion, input, campo) => {
     }
 }
 
-const validarPassword2 = () => {
-    const inputPassword1 = document.getElementById('password');
-    const inputPassword2 = document.getElementById('password2');
+// const validarPassword2 = () => {
+//     const inputPassword1 = document.getElementById('password');
+//     const inputPassword2 = document.getElementById('password2');
 
-    if (inputPassword1.value !== inputPassword2.value) {
-        document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
-        document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
-        document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
-        document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
-        document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-        campos['password'] = false;
-    } else {
-        document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
-        document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-        document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
-        document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-        document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-        campos['password'] = true;
-    }
-}
+//     if (inputPassword1.value !== inputPassword2.value) {
+//         document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
+//         document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
+//         document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
+//         document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
+//         document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+//         campos['password'] = false;
+//     } else {
+//         document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
+//         document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
+//         document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
+//         document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
+//         document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+//         campos['password'] = true;
+//     }
+// }
 
 inputs.forEach((input) => {
     input.addEventListener('keyup', validarFormulario);
